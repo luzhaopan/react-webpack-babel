220c2d19526e88c0fcb91f2d0517035ca1bcd80a
luzhaopan <745003249@qq.com>
2020-05-07 13:56:02 +0800

    react demo create by react webpack babel

---
+++ README.md
@@ -0,0 +1,8 @@
+## create
+
+This project was create by webpack、babel、react.
+
+### `run`
+
+npm install
+npm start
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>react project</h1>
+        <h2>create by webpack、babel、react</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
